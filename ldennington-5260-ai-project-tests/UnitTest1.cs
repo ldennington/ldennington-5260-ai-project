baf2717411c826174ae08bb5ffc0000069d529dc
Lessley Dennington <ldennington@gmail.com>
2022-01-20 23:03:28 -0600

    Add project files.

---
+++ ldennington-5260-ai-project-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ldennington_5260_ai_project_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
