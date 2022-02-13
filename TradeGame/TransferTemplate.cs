473d61aaa498621ece07b8f6b15663ed50daf9d3
Lessley Dennington <ldennington@gmail.com>
2022-02-12 18:09:14 -0800

    Add transfer template

---
+++ TradeGame/TransferTemplate.cs
@@ -0,0 +1,10 @@
+﻿namespace TradeGame
+{
+    internal class TransferTemplate
+    {
+        public string TransferringCountry { get; set; }
+        public string ReceivingCountry { get; set; }
+        public string Resource { get; set; }
+        public int Amount { get; set; }
+    }
+}
